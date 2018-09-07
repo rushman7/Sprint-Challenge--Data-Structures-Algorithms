07e35191cddd6ea88d94ec348ddcbbe199ad3519
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-09-07 11:41:14 -0400

    Init, BST ex_1

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -5,10 +5,26 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-    pass    
+    cb(self.value) # invoke cb with self
+
+    if self.left: # left
+      self.left.depth_first_for_each(cb)
+
+    if self.right: # right
+      self.right.depth_first_for_each(cb)
+
 
   def breadth_first_for_each(self, cb):
-    pass
+    queue = [] # setup a que to check nodes
+    queue.append(self) # first/root node
+
+    while len(queue):
+      current = queue.pop(0) # first in que pulled out
+      if current.left is not None: # if left, put in tree
+        queue.append(current.left)
+      if current.right is not None: # elif, put in right branch
+        queue.append(current.right)
+      cb(current.value) # call's current node and repeats while loop
 
   def insert(self, value):
     new_tree = BinarySearchTree(value)
